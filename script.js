75e5242684d082b6ef551ba668a11d645b57ef64
Sivarasa Nithushna <sivarasanithushna@gmail.com>
2023-11-21 21:53:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+var num=4;
+console.log(num.valueOf())
+var num=-4;
+console.log(num.valueOf())
+var num=0.5;
+console.log(num.valueOf())
+var num=10/5;
+console.log(num.valueOf())
+var num=10/0;
+console.log(num.valueOf())
+var num=4*5;
+console.log(num.valueOf())
+// var num=20 10;
+// console.log(num.valueOf())
+
+// console.log("hello uki");
+// console.error("please submit");
+// document.write("HELLO NITHU");
+// alert(1+2);
+// window.alert("see this");
+// document.getElementById("one").innerHTML=9+9;
+// var firstName="nithu";
+// var lastName="shana";
+// console.log(firstName+" "+lastName);
+// var firstName="shali";
+// console.log(firstName+" "+lastName);
+// let first="shali";
+// let last="shana";
+// console.log(first+" "+last);
+// let can redeclared and not reassigned;
+// let x=5;
+// x=4;
+// console.log(x);
+// const can't reassigned and redeclared;
+// const y=3;
+// const y=5;
+//  y=4;
+
+//  console.log(y);
+//  
+// var z=10;
+// console.log(z+w);
+// var z=20;
+// var w=5;
+// console.log(z+w);
+// block scope
+// var u=20;
+// {
+//     var u=30;  
+//     console.log(u);
+// }
+// console.log(u);
+// let
+// let t=20;
+// {
+//     let t=30;  
+//     console.log(t);
+// }
+// console.log(t);
+// condt
+// const s=20;
+// {
+//  const s=30;  
+//     console.log(s);
+// }
+// console.log(s);
+// hoisting
+// console.log(r);
+// var r=5;
+// let
+// console.log(q);
+// let q=5;
+// let
+// console.log(v);
+// const v=5;
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//   document.getElementById("demo1").innerHTML = fruits;
+//   fruits.push("Kiwi");
+//   document.getElementById("demo2").innerHTML = fruits;
+//   console.log(fruits);
+//   const clothing = ['shoes', 'shirts', 'socks', 'sweaters'];
+// console.log(clothing.length);
+
+// const animals = ['pigs', 'goats', 'sheep'];
+// const count = animals.push('cows');
+// console.log(count);
+// Expected output: 4
+// console.log(animals);
+// Expected output: Array ["pigs", "goats", "sheep", "cows"]
+
+// animals.push('chickens', 'cats', 'dogs');
+// console.log(animals);
+// Expected output: Array ["pigs", "goats", "sheep", "cows", "chickens", "cats", "dogs"]
+
